95803241217a1fa6dd819d0592551233d6588789
Prakash Singh Rajpurohit <61348510+Prakash2101@users.noreply.github.com>
2020-08-08 16:57:41 +0530

    Mail.py

--- Mail.py
+++ Mail.py
@@ -4,31 +4,13 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 import os.path
-
 email = 'taskmessage.21@gmail.com'
 password = 'prakash.s@123'
 send_to_email = 'rajpurohitprakash04@gmail.com'
 subject = '!!! Testing Server Report !!!'
-message = "Web-Server is not running, debug the code and please find the log file in attachment."
-file_location = "/workspace/log.txt"
+message = "Server is not working,please debug the code"
 
 msg = MIMEMultipart()
-msg['From'] = email
-msg['To'] = send_to_email
-msg['Subject'] = subject
-
-msg.attach(MIMEText(message, 'plain'))
-
-# Setup the attachment
-filename = os.path.basename(file_location)
-attachment = open(file_location, "rb")
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-# Attach the attachment to the MIMEMultipart object
-msg.attach(part)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
